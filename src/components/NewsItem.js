4def08ec9bfa28173e2edeacfb6cfd16568f14df
vikasreddy1406 <vikassai345@gmail.com>
2024-01-27 13:09:51 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,48 @@
+import React from "react";
+import bh from "./bh.png";
+
+const NewsItem = (props) => {
+  
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger"> {source} </span>
+          </div>
+          <img
+            src={!imageUrl ? bh : imageUrl}
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title} </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-danger">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default NewsItem;
---
+++ src/components/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import loading from './loading.gif'
+
+const Spinner =()=>{
+    return (
+      <div>
+        <div className="text-center">
+          <img className="my-3" src={loading} alt="loading" />
+        </div>
+      </div>
+    );
+}
+
+export default Spinner
+
+
