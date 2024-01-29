ae3bfa8a6a4689bfe7c9f1fadb9d24b09c94b78b
vikasreddy1406 <vikassai345@gmail.com>
2024-01-30 00:22:38 +0530

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,39 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
-    
-        return (
-            <div >
-                <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    let location = useLocation();
+    useEffect(() => {
+        // console.log(location.pathname);   
+    }, [location]);
+    return (
+        <div >
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                             </li>
-                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-                             
-                          
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/business" ? "active" : ""}`} to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/entertainment" ? "active" : ""}`} to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/general" ? "active" : ""}`} to="/general">General</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/health" ? "active" : ""}`} to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/science" ? "active" : ""}`} to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/sports" ? "active" : ""}`} to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/technology" ? "active" : ""}`} to="/technology">Technology</Link></li>
+
+
                         </ul>
-                        </div>
                     </div>
-                    </nav>
-            </div>
-        )
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default NavBar
